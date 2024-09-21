fb1c5f462ab9ac35d3300a86bfac1fab1f1e0f90
Misliani <misliani1620@gmail.com>
2024-09-21 18:33:22 +0800

    Add files via upload

---
+++ UcapanVar.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tipedata;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class UcapanVar {
+
+    public static void main(String[] args) {
+        String universitas = "Halo ULM ";
+        String ucapan = "ULM Berdaya Saing dan Unggul di Bidang Lahan Basah ";
+        String prodi = "Prodi Ilkom ULM Unggul dengan Teknologi Kekinian ";
+
+        System.out.println(universitas);
+        System.out.println(ucapan);
+        System.out.println(prodi);
+
+    }
+
+}
